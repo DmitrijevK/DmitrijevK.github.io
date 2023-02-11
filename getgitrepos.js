068a675e3598fb66cbd23c802fae5a44537d37c0
DmitrijevK <70750217+DmitrijevK@users.noreply.github.com>
2023-02-11 04:21:16 +0200

    Update getgitrepos.js

--- getgitrepos.js
+++ getgitrepos.js
@@ -1,26 +1,29 @@
 const projectSection = document.querySelector('#project');
 
 fetch('https://api.github.com/users/DmitrijevK/repos')
-.then(res => res.json())
-.then(repos => {
-repos.forEach(repo => {
-const card = document.createElement('div');
-card.className = 'card';
-  const cardHeader = document.createElement('header');
-  cardHeader.className = '.card';
-  cardHeader.innerHTML = `<p class="card-header-title">
-    <a href="${repo.html_url}" target="_blank">${repo.name}</a>
-  </p>`;
-  card.appendChild(cardHeader);
+  .then(res => res.json())
+  .then(repos => {
+    repos.forEach(repo => {
+      const card = document.createElement('div');
+      card.className = 'card';
 
-  const cardContent = document.createElement('div');
-  cardContent.className = 'card-content';
-  cardContent.innerHTML = `<div class="content">
-    ${repo.description || ''}
-  </div>`;
-  card.appendChild(cardContent);
+      const cardHeader = document.createElement('header');
+      cardHeader.className = '.card';
+      cardHeader.innerHTML = `<p class="card-header-title">
+        <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+      </p>`;
+      card.appendChild(cardHeader);
 
-  projectSection.appendChild(card);
-});
-})
-.catch(error => console.error(error));
+      if (repo.description) {
+        const cardContent = document.createElement('div');
+        cardContent.className = 'card-content';
+        cardContent.innerHTML = `<div class="content">
+          ${repo.description}
+        </div>`;
+        card.appendChild(cardContent);
+      }
+
+      projectSection.appendChild(card);
+    });
+  })
+  .catch(error => console.error(error));
