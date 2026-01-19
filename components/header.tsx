4ab61202e211196fab78bec34ddb8fb6abeff7af
DmitrijevK <kirill.dmitrijev0108@gmail.com>
2026-01-19 15:06:36 +0100

    2

--- components/header.tsx
+++ components/header.tsx
@@ -49,9 +49,7 @@ export function Header() {
           </nav>
 
           <div className="flex items-center space-x-4">
-            <div className="hidden md:block">
-              <LanguageSwitcher />
-            </div>
+            <LanguageSwitcher />
 
             <Button variant="ghost" size="icon" className="md:hidden" onClick={() => setIsMenuOpen(!isMenuOpen)}>
               {isMenuOpen ? <X className="w-5 h-5" /> : <Menu className="w-5 h-5" />}
@@ -83,9 +81,6 @@ export function Header() {
                   </Link>
                 )
               ))}
-              <div className="pt-4 border-t border-border">
-                <LanguageSwitcher />
-              </div>
             </nav>
           </div>
         )}
